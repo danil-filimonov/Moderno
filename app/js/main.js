58b68126e399f19846178a0ba22ad7fc1807b5e5
danil-filimonov <lsamba99@gmail.com>
2020-02-15 00:00:41 +0700

    add section products-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
-$(function(){
+$(function () {
+
+    $('.rate-stars').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        ratedFill: '#FFA726',
+        readOnly: true
+    });
+
+    $('.products__slider-inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 
     var mixer = mixitup('.products__inner-box');
 
