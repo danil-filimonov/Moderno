9e2ce3d3f85be8a7f769bfb38bbe28b9dcc37711
danil-filimonov <lsamba99@gmail.com>
2020-02-29 00:26:19 +0700

    add file new-items, add aside-bar

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,17 @@ $(function () {
         slidesToScroll: 4
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        skin: 'round',
+        prefix: '$',
+        hide_min_max: true
+    });
+
     var mixer = mixitup('.products__inner-box');
 
 });
