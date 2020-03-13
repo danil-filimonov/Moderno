70ec3510c32b705fdcbc830991daf8772aced8e4
danil-filimonov <lsamba99@gmail.com>
2020-03-14 01:46:35 +0700

    add product-one.html

--- app/js/main.js
+++ app/js/main.js
@@ -25,21 +25,30 @@ $(function () {
         hide_min_max: true
     });
 
-    $('.icon-th-list').on('click', function(){
+    $('.icon-th-list').on('click', function () {
         $('.product-page__items > .product__item').addClass('list');
         $('.icon-th-large').removeClass('filter-active');
         $('.icon-th-list').addClass('filter-active');
     });
 
-    $('.icon-th-large').on('click', function(){
+    $('.icon-th-large').on('click', function () {
         $('.product-page__items > .product__item').removeClass('list');
         $('.icon-th-large').addClass('filter-active');
         $('.icon-th-list').removeClass('filter-active');
     });
 
-    $('.menu__btn').on('click', function(){
+    $('.menu__btn').on('click', function () {
         $('.menu__list').slideToggle();
         $('.menu__btn').toggleClass('active');
+    });
+
+    $('.product-one__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
     });
 
     var mixer = mixitup('.products__inner-box');
